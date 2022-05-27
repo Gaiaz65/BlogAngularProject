f51abb930da8e0c2b35bca550f1c7eb16249bede
Gaiaz65 <gayazapkaev365@gmail.com>
2022-05-27 13:32:00 +0400

    The Authentication via firebase has been added. Authentication works good, except for the logout.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apikey: 'AIzaSyAhs268MOLArI0_JWt0aTcUZ8PTjnj-IQ0',
 };
