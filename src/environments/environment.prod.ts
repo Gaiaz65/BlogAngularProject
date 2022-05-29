cdd5810e7ccee4acd0ddfb6221c641124e8d5853
Gaiaz65 <gayazapkaev365@gmail.com>
2022-05-29 14:23:26 +0400

    Post creation block has been added. Authorisation with FB works correctly.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
-import { Environment } from "./interface";
+import { Environment } from './interface';
 
 export const environment: Environment = {
   production: true,
   apikey: 'AIzaSyAhs268MOLArI0_JWt0aTcUZ8PTjnj-IQ0',
+  fbDbUrl: 'https://blog-project-angular-default-rtdb.firebaseio.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ import { Environment } from "./interface";
 export const environment: Environment = {
   production: false,
   apikey: 'AIzaSyAhs268MOLArI0_JWt0aTcUZ8PTjnj-IQ0',
+  fbDbUrl: 'https://blog-project-angular-default-rtdb.firebaseio.com',
 };
 
 /*
